182b8523f177fc03d8f1e247b7214582a9640a0f
sina <sina.majdieh@gmail.com>
2020-05-15 19:07:26 +0430

    files package

---
+++ pkg/filePkg/files.go
@@ -0,0 +1,49 @@
+package filePkg
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	ArticlesPath            = "Articles"
+	GlobalArticlesDirectory = ArticlesPath + "/Glob"
+	AttractionsDirectory    = ArticlesPath + "/Attractions"
+	IntroDirectory          = ArticlesPath + "/Intro"
+)
+
+func MakePath(dir []string, file string, format string) string {
+	Dir := ""
+	for _, v := range dir {
+		Dir += v + "/"
+	}
+	return Dir + file + "." + format
+}
+func ReadJson(path string, v interface{}) error {
+	jsonFile, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue, v)
+	return nil
+}
+
+// func ReadArticle(name string) (*Article, error) {
+// 	jsonFile, err := os.Open(AticlesDirectory + name + ".json")
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer jsonFile.Close()
+// 	byteValue, _ := ioutil.ReadAll(jsonFile)
+// 	var article Article
+// 	json.Unmarshal(byteValue, &article)
+// 	return &article, nil
+// }
+
+func ListDirectory(path string) ([]os.FileInfo, error) {
+	return ioutil.ReadDir(path)
+
+}
