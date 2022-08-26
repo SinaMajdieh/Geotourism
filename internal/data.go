11245643574473ebadec3e85e12db17d0f9c344c
Sina <sina.majdieh@gmail.com>
2022-08-26 20:44:57 +0430

    fixed typo.

--- internal/data.go
+++ internal/data.go
@@ -3,19 +3,18 @@ package internal
 import (
 	"github.com/SinaMajdieh/Geotourism/pkg/domModel"
 	"github.com/SinaMajdieh/Geotourism/pkg/tourson"
-	"strings"
 )
 
 func Load_attractions() {
 	attractionsList, attractions = tourson.Load_attractions_list_files(phenomena)
 }
 func Update_attractions_list(att *domModel.Attraction) {
-	att.Value = strings.Replace(att.Phenomena, " ", "_", -1)
+	/*att.Value = strings.Replace(att.Phenomena, " ", "_", -1)
 	attractionsList.Map[att.Value].Attractions = append(attractionsList.Map[att.Value].Attractions, *att)
 	attractionsList.Map[att.Value].Count++
 
 	attractions.Attractions = append(attractions.Attractions, *att)
-	attractions.Count++
+	attractions.Count++*/
 }
 func Load_intros() {
 	intros = tourson.LoadIntroListFiles(DocList)
